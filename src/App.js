8179c4af94af728e794235f40ab70205010457f6
Valerii Viun <2455521@gmail.com>
2023-11-21 16:28:26 +0200

    finish

--- src/App.js
+++ src/App.js
@@ -74,13 +74,14 @@ export default function Game() {
   const moves = history.map((squares, move) => {
     let description;
     if (move > 0) {
-      description = 'Go to move #' + move;
+      description = 'You are at move #' + move;
     } else {
       description = 'Go to game start';
     }
     return (
       <li key={move}>
-        <button onClick={() => jumpTo(move)}>{description}</button>
+        {/* <button onClick={() => jumpTo(move)}>{description}</button> */}
+        <>You are at move #{move}</>
       </li>
     );
   });
